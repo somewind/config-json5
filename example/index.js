7dc4c6626478588a86c4516c74becf0f40644cd3
maoshuchen(陈津) <maoshuchen@tencent.com>
2019-06-26 22:20:31 +0800

    feat: compatible with strict mode

--- example/index.js
+++ example/index.js
@@ -7,10 +7,10 @@ console.log(`========== env: ${process.env.NODE_ENV} ==========`)
 console.log(config)
 
 console.log(`========== env: com.somewind.domain1.dev ==========`)
-console.log(config(configPath, 'com.somewind.domain1.dev'))
+console.log(config.parse(configPath, 'com.somewind.domain1.dev'))
 
 console.log(`========== env: domain1.dev ==========`)
-console.log(config(configPath, 'domain1.dev'))
+console.log(config.parse(configPath, 'domain1.dev'))
 
 console.log(`========== env: pro ==========`)
-console.log(config(configPath, 'pro'))
+console.log(config.parse(configPath, 'pro'))
