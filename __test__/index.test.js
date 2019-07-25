ad53cd40591e9957f67afd9dffdcebe9c9d3e41c
maoshuchen(陈津) <maoshuchen@tencent.com>
2019-07-25 22:03:58 +0800

    test: and test support

---
+++ __test__/index.test.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const config = require('../')
+
+const configPath = path.join(__dirname, '../example/config')
+
+test('config load empty env=undefined', () => {
+  expect(JSON.stringify(config.parse(configPath, undefined)))
+    .toBe('{"name":"default.json","advanced/default.js":1,"default.json5":1,"default.json":1}')
+})
+
+test('config load fullname env=com.somewind.domain1.dev', () => {
+  expect(JSON.stringify(config.parse(configPath, 'com.somewind.domain1.dev')))
+    .toBe('{"name":"default.json","advanced/default.js":1,"advanced/com/default.js":1,"advanced/com/somewind.json":1,"advanced/com.somewind.json":1,"advanced/com/somewind/default.js":1,"advanced/com.somewind/default.js":1,"advanced/com.somewind/domain1.js":1,"advanced/com.somewind/domain1.dev.json":1,"default.json5":1,"default.json":1}')
+})
+
+test('config load subname env=domain1.dev', () => {
+  expect(JSON.stringify(config.parse(configPath, 'domain1.dev')))
+    .toBe('{"name":"default.json","advanced/default.js":1,"advanced/com/default.js":1,"advanced/com/somewind.json":1,"advanced/com.somewind.json":1,"advanced/com/somewind/default.js":1,"advanced/com.somewind/default.js":1,"advanced/com.somewind/domain1.js":1,"advanced/com.somewind/domain1.dev.json":1,"default.json5":1,"default.json":1}')
+})
+
+test('config load not exist env=pro', () => {
+  expect(JSON.stringify(config.parse(configPath, 'pro')))
+    .toBe('{"name":"default.json","advanced/default.js":1,"default.json5":1,"default.json":1}')
+})
