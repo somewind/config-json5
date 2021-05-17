493776d40c957b8d066f29bff42162e9661f9ce5
somewind <neverstopsteps@gmail.com>
2021-05-17 21:26:39 +0800

    fix: add default.yaml

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -7,12 +7,12 @@ const configPath = path.join(__dirname, '../example/config')
 
 test('config load fullname env=com.somewind.domain1.dev', () => {
   expect(JSON.stringify(config.parse(configPath, 'com.somewind.domain1.dev')))
-    .toBe('{"name":"default.json","advanced/default.js":1,"advanced/com/default.js":1,"advanced/com/somewind.json":1,"advanced/com.somewind.json":1,"advanced/com.somewind.yaml":1,"advanced/com/somewind/default.js":1,"part1":{"@part1.js":"none"},"part2":{"@part2.js":1},"@.js":1,"part3":{"@part3.json5":1,"@part3.json":1},"@.json5":1,"@.json":1,"advanced/com.somewind/default.js":1,"advanced/com.somewind/domain1.js":1,"advanced/com.somewind/domain1.dev.json":1,"default.json5":1,"default.json":1}')
+    .toBe('{"name":"default.json","advanced/default.js":1,"advanced/com/default.js":1,"advanced/com/somewind.json":1,"advanced/com.somewind.json":1,"advanced/com.somewind.yaml":1,"advanced/com/somewind/default.js":1,"advanced/com/somewind/default.yaml":1,"part1":{"@part1.js":"none"},"part2":{"@part2.js":1},"@.js":1,"part3":{"@part3.json5":1,"@part3.json":1},"@.json5":1,"@.json":1,"advanced/com.somewind/default.js":1,"advanced/com.somewind/domain1.js":1,"advanced/com.somewind/domain1.dev.json":1,"default.json5":1,"default.json":1}')
 })
 
 test('config load startname env=com.somewind', () => {
   expect(JSON.stringify(config.parse(configPath, 'com.somewind')))
-    .toBe('{"name":"default.json","advanced/default.js":1,"advanced/com/default.js":1,"advanced/com/somewind.json":1,"advanced/com.somewind.json":1,"advanced/com.somewind.yaml":1,"advanced/com/somewind/default.js":1,"part1":{"@part1.js":"none"},"part2":{"@part2.js":1},"@.js":1,"part3":{"@part3.json5":1,"@part3.json":1},"@.json5":1,"@.json":1,"advanced/com.somewind/default.js":1,"default.json5":1,"default.json":1}')
+    .toBe('{"name":"default.json","advanced/default.js":1,"advanced/com/default.js":1,"advanced/com/somewind.json":1,"advanced/com.somewind.json":1,"advanced/com.somewind.yaml":1,"advanced/com/somewind/default.js":1,"advanced/com/somewind/default.yaml":1,"part1":{"@part1.js":"none"},"part2":{"@part2.js":1},"@.js":1,"part3":{"@part3.json5":1,"@part3.json":1},"@.json5":1,"@.json":1,"advanced/com.somewind/default.js":1,"default.json5":1,"default.json":1}')
 })
 
 test('config load startname env=com', () => {
@@ -22,7 +22,7 @@ test('config load startname env=com', () => {
 
 test('config load subname env=domain1.dev', () => {
   expect(JSON.stringify(config.parse(configPath, 'domain1.dev')))
-    .toBe('{"name":"default.json","advanced/default.js":1,"advanced/com/default.js":1,"advanced/com/somewind.json":1,"advanced/com.somewind.json":1,"advanced/com.somewind.yaml":1,"advanced/com/somewind/default.js":1,"part1":{"@part1.js":"none"},"part2":{"@part2.js":1},"@.js":1,"part3":{"@part3.json5":1,"@part3.json":1},"@.json5":1,"@.json":1,"advanced/com.somewind/default.js":1,"advanced/com.somewind/domain1.js":1,"advanced/com.somewind/domain1.dev.json":1,"default.json5":1,"default.json":1}')
+    .toBe('{"name":"default.json","advanced/default.js":1,"advanced/com/default.js":1,"advanced/com/somewind.json":1,"advanced/com.somewind.json":1,"advanced/com.somewind.yaml":1,"advanced/com/somewind/default.js":1,"advanced/com/somewind/default.yaml":1,"part1":{"@part1.js":"none"},"part2":{"@part2.js":1},"@.js":1,"part3":{"@part3.json5":1,"@part3.json":1},"@.json5":1,"@.json":1,"advanced/com.somewind/default.js":1,"advanced/com.somewind/domain1.js":1,"advanced/com.somewind/domain1.dev.json":1,"default.json5":1,"default.json":1}')
 })
 
 test('config load not exist env=pro', () => {
@@ -42,5 +42,5 @@ test('config load empty env=undefined, args=["hello", 123]', () => {
 
 test('config load empty env=domain1.dev, args=["hello", 123]', () => {
   expect(JSON.stringify(config.parse(configPath, 'domain1.dev', ['hello', 123])))
-    .toBe('{"name":"default.json","advanced/default.js":1,"args":"hello 123","advanced/com/default.js":1,"advanced/com/somewind.json":1,"advanced/com.somewind.json":1,"advanced/com.somewind.yaml":1,"advanced/com/somewind/default.js":1,"part1":{"@part1.js":"args"},"part2":{"@part2.js":1},"@.js":1,"part3":{"@part3.json5":1,"@part3.json":1},"@.json5":1,"@.json":1,"advanced/com.somewind/default.js":1,"advanced/com.somewind/domain1.js":1,"advanced/com.somewind/domain1.dev.json":1,"default.json5":1,"default.json":1}')
+    .toBe('{"name":"default.json","advanced/default.js":1,"args":"hello 123","advanced/com/default.js":1,"advanced/com/somewind.json":1,"advanced/com.somewind.json":1,"advanced/com.somewind.yaml":1,"advanced/com/somewind/default.js":1,"advanced/com/somewind/default.yaml":1,"part1":{"@part1.js":"args"},"part2":{"@part2.js":1},"@.js":1,"part3":{"@part3.json5":1,"@part3.json":1},"@.json5":1,"@.json":1,"advanced/com.somewind/default.js":1,"advanced/com.somewind/domain1.js":1,"advanced/com.somewind/domain1.dev.json":1,"default.json5":1,"default.json":1}')
 })
